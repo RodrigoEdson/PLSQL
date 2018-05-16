c11aae7388bffffae5f0f0e10a0fb7aa6ff211ed
Rodrigo Edson Fernandes <rodrigoedson@gmail.com>
2018-05-16 07:43:37 -0300

    DBMS_ALERT usage test

---
+++ Samples/Parallelism/DBMS_ALERT/script_ALERT_listener.sql
@@ -0,0 +1,34 @@
+DECLARE
+  vStatus  PLS_INTEGER;
+  vMsgText VARCHAR2(100);
+  CONST_ALERT_NAME CONSTANT VARCHAR2(10) := 'TEST_ALERT';
+  CONST_TIMEOUT    CONSTANT PLS_INTEGER := 5;
+  --
+  noMoreMessages EXCEPTION;
+  PRAGMA EXCEPTION_INIT(noMoreMessages, -6556);
+BEGIN
+  /*
+  Register interest in an alert.
+  */
+  dbms_alert.register(CONST_ALERT_NAME);
+  /*
+  get Pipe message
+  */
+  dbms_alert.waitone(NAME    => CONST_ALERT_NAME,
+                     message => vMsgText,
+                     status  => vStatus,
+                     timeout => CONST_TIMEOUT);
+  IF vStatus = 1 THEN
+    dbms_output.put_line('ERROR: Alert timeout');
+  ELSE
+    /*
+    Show message
+    */
+    dbms_output.put_line(vMsgText);
+  END IF;
+  /*
+  Remove alert from registration list.
+  */
+  dbms_alert.register(CONST_ALERT_NAME);
+  --
+END;
